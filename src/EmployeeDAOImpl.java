c49fbcef25d1fce0eff3d6db0912ff948b5fb2cc
supawan <supawann1999@gmail.com>
2021-03-23 12:13:27 +0700

    Updata

--- src/EmployeeDAOImpl.java
+++ src/EmployeeDAOImpl.java
@@ -14,12 +14,11 @@ public class EmployeeDAOImpl implements EmployeeDAO{
     public static final String ADD_NEW_EMP = "insert into Employee (emp_id,name,position,email,salary) " +
             "values(?,?,?,?,?)";
     public static final String FIND_EMP_BY_ID = "select * from Employee where emp_id = ?";
-    public static final String UPDATE_EMP = "update Employee"+
-            "set name= ?,"+
-            "position= ?,"+
-            "email= ?, "+
-            "salary= ?, "+
-            "where emp_id = ?,";
+    public static final String UPDATE_EMP = "update Employee set name = ? , position = ? , email = ? , salary = ? " +
+            "where emp_id = ?";
+    public static final String DELETE_EMP = "delete from Employee where emp_id = ? ";
+
+
 
     //Create Instants
     private static EmployeeDAOImpl instance = new EmployeeDAOImpl();
@@ -128,32 +127,55 @@ public class EmployeeDAOImpl implements EmployeeDAO{
         try {
             conn = DriverManager.getConnection(url);
             PreparedStatement ps = conn.prepareStatement(UPDATE_EMP);
-            //set par
-            ps.setString(1,employee.getName());
-            ps.setString(2,employee.getPosition());
-            ps.setString(3,employee.getEmail());
-            ps.setDouble(4,employee.getSalary());
-            ps.setString(5,employee.getEmp_id());
 
-            int rs = ps.executeUpdate(); //return 0 or 1
-            if (rs !=0){//update
-                System.out.println("Employee with id"+employee.getEmp_id()+"was updated.");
-            }else { //not update
-                System.out.println("Could not update employee with id"+employee.getEmp_id());
+            //set parameter
+            ps.setString(1, employee.getName());
+            ps.setString(2, employee.getPosition());
+            ps.setString(3, employee.getEmail());
+            ps.setDouble(4, employee.getSalary());
+            ps.setString(5, employee.getEmp_id());
+
+            int rs = ps.executeUpdate(); //return 0 1
+            if(rs != 0){
+                System.out.println("Employee with id "+employee.getEmp_id()+" was update");
             }
-            //close connection
+            else {
+                System.out.println("Could not update employee with id " + employee.getEmp_id());
+            }
+
+            //close conn
+            ps.close();
+            conn.close();
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void deleteEmp(String id) {
+        try {
+            conn = DriverManager.getConnection(url);
+            PreparedStatement ps = conn.prepareStatement(DELETE_EMP);
+
+            //set parameter
+            ps.setString(1, id);
+
+            int rs = ps.executeUpdate();
+            if (rs != 0){
+                System.out.println("Employee data with id "+id+" was deleted");
+            } else {
+                System.out.println("Could not find employee id : "+id);
+            }
+
             ps.close();
             conn.close();
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-
-    }
-
-    @Override
-    public void deleteEmp(String id) {
 
     }
 }
